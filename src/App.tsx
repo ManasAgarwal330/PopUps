1286f9af0d718de7dcafecc9c53dae2fc1f6c72e
ManasAgarwal330 <agarwalmanas212001@gmail.com>
2021-08-05 15:44:11 -0700

    changed App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import { useState } from 'react';
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className="App">
+    <div className='absolute inset-0 justify-center flex items-center bg-gray-200'>
+      <span className='text-lg text-blue-700 font-semibold cursor-pointer'
+        onClick={() => setIsOpen(!isOpen)}>Click to Open Confirm Modal</span>
     </div>
   );
 }
